2cf3b058ee6cda33d06885ced10bea13703e078b
slorello <steve.lorello@redislabs.com>
2022-08-17 09:38:49 -0400

    first commit

---
+++ TestRHELdotnet6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.Security;
+using System.Security.Authentication;
+using StackExchange.Redis;
+
+var options = new ConfigurationOptions();
+options.EndPoints.Add("your_endpoint");
+options.Ssl = true;
+options.Password = "your_password";
+// options.SslProtocols = SslProtocols.Tls12;
+
+options.CertificateValidation += (sender, certificate, chain, errors) =>
+{
+    Console.WriteLine(errors);
+    return true;
+};
+
+var muxer = ConnectionMultiplexer.Connect(options);
+var db = muxer.GetDatabase();
+
+db.StringSet("foo", "bar");
+Console.WriteLine("Completed. . .");
---
+++ dockerfile
@@ -0,0 +1,15 @@
+FROM ubuntu:bionic
+
+RUN apt-get update
+RUN apt-get install wget -y
+RUN wget https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+RUN dpkg -i packages-microsoft-prod.deb
+RUN rm packages-microsoft-prod.deb
+RUN apt-get update && \
+  apt-get install -y dotnet-sdk-6.0
+
+WORKDIR /app
+ADD . /app
+ENV SERedis_EnabledCiphers=TLS_RSA_WITH_AES_128_CBC_SHA256
+# RUN dotnet restore && dotnet build
+ENTRYPOINT [ "dotnet", "run", "--project",  "./TestRHELdotnet6/TestRHELdotnet6.csproj"]
